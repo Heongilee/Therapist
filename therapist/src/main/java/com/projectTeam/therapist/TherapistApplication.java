d1cbdafa66b8119b0917143ec3644568b0420a63
Heongilee <gjsrl1@gmail.com>
2021-07-17 10:38:58 +0900

    heongilee 스프링 프로젝트 초기 설정

---
+++ therapist/src/main/java/com/projectTeam/therapist/TherapistApplication.java
@@ -0,0 +1,13 @@
+package com.projectTeam.therapist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherapistApplication.class, args);
+	}
+
+}
