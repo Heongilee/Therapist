8a5f6e33aec23c47556e05bb10f09d27f2f9f9d0
Heongilee <gjsrl1@gmail.com>
2021-07-17 16:00:52 +0900

    heongilee :: Thymeleaf 익히기 + common.html로 네비바 만들어보기

---
+++ therapist/src/main/java/com/projectTeam/therapist/boardService/BoardController.java
@@ -0,0 +1,14 @@
+package com.projectTeam.therapist.boardService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/list")
+    public String list() {
+        return "board/list";
+    }
+}
