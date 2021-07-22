2626576939d9df0b3ed9be452885c6dcdea8c80a
Heongilee <gjsrl1@gmail.com>
2021-07-22 11:23:41 +0900

    feature-comment, 댓글Dto랑 BoardSe rvice 그리고 BoardServiceImpl 추가

---
+++ therapist/src/main/java/com/projectTeam/therapist/model/commentDto.java
@@ -0,0 +1,32 @@
+package com.projectTeam.therapist.model;
+
+public class commentDto {
+    private Long commentId;
+    private Long memberId;
+    private String commentContent;
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
+
+}
