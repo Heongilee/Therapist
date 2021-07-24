289c8bd58b86a0196c842b0f2906db773660276a
Heongilee <gjsrl1@gmail.com>
2021-07-24 10:48:02 +0900

    feature-postValidation :: 제목 글자수 검증 추가

--- therapist/src/main/java/com/projectTeam/therapist/boardService/PostController.java
+++ therapist/src/main/java/com/projectTeam/therapist/boardService/PostController.java
@@ -5,8 +5,10 @@ import com.projectTeam.therapist.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -23,18 +25,21 @@ public class PostController {
     }
 
     @GetMapping("/form")
-    public String form(Model model, @RequestParam(required = false) Long id) {
-        if (id == null) {
+    public String form(Model model, @RequestParam(required = false) Long postId) {
+        if (postId == null) {
             model.addAttribute("post", new PostDto());
         } else {
-            PostDto postDto = postRepository.findById(id).orElse(null);
+            PostDto postDto = postRepository.findById(postId).orElse(null);
             model.addAttribute("post", postDto);
         }
         return "post/form";
     }
     @PostMapping("/form")
-    public String formSubmit(@ModelAttribute PostDto postDto, Model model) {
-        postRepository.save(postDto);
+    public String formSubmit(@ModelAttribute("post") @Valid PostDto post, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "post/form";
+        }
+        postRepository.save(post);
         return "redirect:/post/list";
     }
 }
