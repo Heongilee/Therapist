883ba894046fa2271572c759a096969bdca131ec
Heongilee <gjsrl1@gmail.com>
2021-07-26 14:55:35 +0900

    feature-apiComment :: 댓글 RestApi 구현 완료

--- therapist/src/main/java/com/projectTeam/therapist/repository/CommentRepository.java
+++ therapist/src/main/java/com/projectTeam/therapist/repository/CommentRepository.java
@@ -1,8 +1,11 @@
 package com.projectTeam.therapist.repository;
 
 import com.projectTeam.therapist.model.CommentDto;
-import com.projectTeam.therapist.model.PostDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<CommentDto, Long>{
+    List<CommentDto> findByPostId(Long postId);
+    List<CommentDto> findByCommentId(Long commentId);
 }
