8139f463031321b20465be4f781113a6f2ea5777
yura <jungyr24@hanmail.net>
2021-07-27 11:04:50 +0900

    feature-reply :: 답글(reply)관련 API 구현 (#22)
    * feature-reply :: 답글(reply)관련 API 구현
    
    * feature-reply :: 답글(reply)관련 API 구현

---
+++ therapist/src/main/java/com/projectTeam/therapist/restService/ReplyApiController.java
@@ -0,0 +1,54 @@
+package com.projectTeam.therapist.restService;
+
+import com.projectTeam.therapist.boardService.BoardServiceImpl;
+import com.projectTeam.therapist.model.ReplyDto;
+import com.projectTeam.therapist.repository.ReplyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reply")
+public class ReplyApiController {
+    @Autowired
+    private ReplyRepository replyRepository;
+    private BoardServiceImpl boardServiceImpl;
+    @Autowired
+    public ReplyApiController(BoardServiceImpl boardServiceImpl) {
+        this.boardServiceImpl = boardServiceImpl;
+    }
+
+    // postId에 따른 답글 조회
+    @GetMapping("")
+    List<ReplyDto> findReplies(@RequestParam(required = false, defaultValue = "0") Long postId) {
+        return replyRepository.findByPostId(postId);
+    }
+
+    // 답글 생성
+    @PostMapping("/new")
+    ReplyDto newReply(@RequestBody ReplyDto newReply) {
+        return replyRepository.save(newReply);
+    }
+
+    // 답글 수정
+    @PutMapping("/{replyId}")
+    ReplyDto modifyReply(@RequestBody() ReplyDto modifiedReply,
+                         @PathVariable Long replyId) {
+        return replyRepository.findById(replyId)
+                .map(replyDto -> {
+                    replyDto.setReplyContent(modifiedReply.getReplyContent());
+                    return replyRepository.save(replyDto);
+                })
+                .orElseGet(() -> {
+                    modifiedReply.setReplyId(replyId);
+                    return replyRepository.save(modifiedReply);
+                });
+    }
+
+    // 답글 삭제
+    @DeleteMapping("/{replyId}")
+    void deleteReply(@PathVariable Long replyId) {
+        replyRepository.deleteById(replyId);
+    }
+}
