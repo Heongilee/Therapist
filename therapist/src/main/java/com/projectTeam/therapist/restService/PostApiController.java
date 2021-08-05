53c9c48674fe435c513ad3ab75d9236c1152bc2c
Heongilee <gjsrl1@gmail.com>
2021-08-05 12:52:24 +0900

    (수정 필요)PostType에 따른 게시글 조회 + 총 게시글 수 + Pagenation api 요청

--- therapist/src/main/java/com/projectTeam/therapist/restService/PostApiController.java
+++ therapist/src/main/java/com/projectTeam/therapist/restService/PostApiController.java
@@ -5,7 +5,6 @@ import com.projectTeam.therapist.model.PostDto;
 import com.projectTeam.therapist.postService.PostService;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
