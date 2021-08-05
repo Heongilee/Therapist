8e4f9f0e40205c46216b9a392bca27afbe37b972
Heongilee <gjsrl1@gmail.com>
2021-08-05 21:41:03 +0900

    (수정완료) postType과 page번호를 이용한 게시글 조회 API

--- therapist/src/main/java/com/projectTeam/therapist/restService/PostApiController.java
+++ therapist/src/main/java/com/projectTeam/therapist/restService/PostApiController.java
@@ -1,96 +1,48 @@
 package com.projectTeam.therapist.restService;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.projectTeam.therapist.model.PostCategory;
 import com.projectTeam.therapist.model.PostDto;
-import com.projectTeam.therapist.repository.PostRepository;
-import com.projectTeam.therapist.repository.ReplyRepository;
-import org.json.simple.JSONArray;
+import com.projectTeam.therapist.postService.PostService;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.print.Pageable;
-import java.util.List;
 
 // `@RestController`어노테이션을 사용하는 경우, 요청과 응답의 객체변환 및 직렬화/역직렬화를 자동으로 이 jackson 라이브러리가 담당하게 된다.
 @RestController
 @RequestMapping("/api")
 class PostApiController {
     @Autowired
-    private PostRepository postRepository;
-    @Autowired
-    private ReplyRepository replyRepository;
+    private PostService postService;
 
-    // 카테고리(postType)와 일치하는 모든 게시글을 조회하는 요청
-//    class RequestGetDto {
-//        private int postLength;
-//        private List<PostDto> posts;
-//
-//        public RequestGetDto(int postLength, List<PostDto> posts) {
-//            this.postLength = postLength;
-//            this.posts = posts;
-//        }
-//    }
-
-    // TODO : Pageable객체를 이용한 페이징 처리 (Therapist_experiemnt 브랜치 아카이브 보고서 해결할 것.)
     @GetMapping("/posts")
-    JSONObject requestGet(@RequestParam(required = false, defaultValue="JOB") PostCategory postType){
-        List<PostDto> posts = postRepository.findByPostType(postType);
-
-        // 검색된 카테고리 게시글 개수
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("postType", postType);
-        jsonObject.put("totalPage", postRepository.countByPostType(postType));
-
-        // postType 파라미터와 일치하는 모든 게시글들을 JSONArray로 담는다.
-        JSONArray postsArray = new JSONArray();
-        for (PostDto post : posts) {
-            JSONObject item = new JSONObject();
-            item.put("postId", post.getPostId());
-            item.put("userId", post.getUserDto().getUserId());
-            item.put("postType", post.getPostType().toString()); // Enum 타입이기 때문에 문자열 처리를 위한 .toString() 을 붙여준다.
-            item.put("postTitle", post.getPostTitle());
-            item.put("postContent", post.getPostContent());
-
-            item.put("replyLength", replyRepository.countByPostId(post.getPostId()));
-            postsArray.add(item);
-        }
-        jsonObject.put("posts", postsArray);
-        return jsonObject;
+    JSONObject requestGet(@RequestParam(required = false, defaultValue="JOB") PostCategory postType, @PageableDefault(size = 6) final Pageable pageable){
+        return postService.findByPostType(postType, pageable);
     }
 
     @PostMapping("/posts")
     PostDto newPost(@RequestBody PostDto newPost) {
-        return postRepository.save(newPost);
+        return postService.save(newPost);
     }
 
     // Single item
     @GetMapping("/posts/{postId}")
     PostDto one(@PathVariable Long postId) {
-        return postRepository.findById(postId).orElse(null);
+        return postService.findSingleItem(postId);
+//
     }
 
     @PutMapping("/posts/{id}")
     PostDto replacePost(@RequestBody PostDto newPost, @PathVariable Long id) {
+        return postService.modifyById(newPost, id);
 
-        return postRepository.findById(id)
-                .map(postDto -> {
-                    postDto.setPostType(newPost.getPostType());
-                    postDto.setPostTitle(newPost.getPostTitle());
-                    postDto.setPostContent(newPost.getPostContent());
-                    return postRepository.save(postDto);
-                })
-                .orElseGet(() -> {
-                    newPost.setPostId(id);
-                    return postRepository.save(newPost);
-                });
     }
 
     @DeleteMapping("/posts/{id}")
-    void deletePost(@PathVariable Long id) {
-        postRepository.deleteById(id);
+    void deletePost(@PathVariable Long postId) {
+        postService.deleteById(postId);
+//
     }
 }
