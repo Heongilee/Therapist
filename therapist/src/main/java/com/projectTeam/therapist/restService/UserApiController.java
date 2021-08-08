6610fd3b8991233f5ff5771c5bd083b9d50b0e37
Heongi Lee <55871242+Heongilee@users.noreply.github.com>
2021-08-08 15:21:52 +0900

    feature-delegatingService :: 리팩토링 + 일부 API들 수정. (#38)
    * feature-delegatingService :: UserApiController의 비즈니스 로직을 Service객체에게 위임.
    
    * feature-delegatingService :: PostCommentApiController 다시 작성 + commentService로 비즈니스 로직을 위임
    
    * feature-delegatingService :: postType에 따른 Pageable 게시글 조회 API 수정(댓글 필드 삭제)

--- therapist/src/main/java/com/projectTeam/therapist/restService/UserApiController.java
+++ therapist/src/main/java/com/projectTeam/therapist/restService/UserApiController.java
@@ -5,7 +5,6 @@ import com.projectTeam.therapist.userService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -48,13 +47,9 @@ class UserApiController {
         userService.deleteMyPosts(posts);
     }
 
-
     // 마이페이지 체크 박스에 따른 게시글 / 댓글 삭제
     @PostMapping("/users/mypage")
     void deleteMyPosts(@RequestBody Map<Long, Long> posts) {
-        System.out.println(posts);
-        for (Long postId : posts.values()) {
-            postRepository.deleteById(postId);
-        }
+        userService.deleteMyPosts(posts);
     }
 }
