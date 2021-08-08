f625cc04480b2b62650012de3efe7d1226757f9c
Heongilee <gjsrl1@gmail.com>
2021-08-08 15:19:02 +0900

    feature-delegatingService :: postType에 따른 Pageable 게시글 조회 API 수정(댓글 필드 삭제)

--- therapist/src/main/java/com/projectTeam/therapist/restService/CommentApiController.java
+++ therapist/src/main/java/com/projectTeam/therapist/restService/CommentApiController.java
@@ -5,6 +5,7 @@ import com.projectTeam.therapist.model.CommentDto;
 import com.projectTeam.therapist.model.PostCommentDto;
 import com.projectTeam.therapist.postService.CommentService;
 import com.projectTeam.therapist.repository.CommentRepository;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,19 +29,25 @@ class CommentApiController {
 
     // 게시글 댓글(PostComment)의 아이디에 해당하는 게시글 댓글 조회
     @GetMapping("/postComments/{postCommentId}")
-    PostCommentDto findSinglePostComment(Long postCommentId) {
+    PostCommentDto findSinglePostComment(@PathVariable Long postCommentId) {
         return commentService.findSinglePostComment(postCommentId);
     }
 
     // 게시글 댓글(PostComment) 생성
-    @PostMapping("/postComments")
-    PostCommentDto newPostComment(@RequestBody PostCommentDto newPostComment) {
-        return commentService.newPostComment(newPostComment);
+    @PostMapping("/postComments/{userName}/{postId}")
+    PostCommentDto newPostComment(@RequestBody JSONObject params, @PathVariable String userName, @PathVariable Long postId) {
+        return commentService.newPostComment(params, userName, postId);
     }
 
     // 게시글 댓글(PostComment) 삭제
-    @DeleteMapping("/postComments")
-    void deletePostComment(Long postCommentId) {
+    @DeleteMapping("/postComments/{postCommentId}")
+    void deletePostComment(@PathVariable Long postCommentId) {
         commentService.deletePostComment(postCommentId);
     }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // ReplyComment에 관한 API
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
