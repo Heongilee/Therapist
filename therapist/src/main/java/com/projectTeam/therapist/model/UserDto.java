8e850d08900575207ec9c7074bdb4a8e62925b2a
Heongilee <gjsrl1@gmail.com>
2021-08-10 14:34:55 +0900

    feature-addThumbnailAvatar :: 로그인 유저 프로필, 썸네일 정보 가져오기 (1) - UserDetails(org.springframework.security.core.userdetails.UserDetails)를 구현한 UserDto 클래스

--- therapist/src/main/java/com/projectTeam/therapist/model/UserDto.java
+++ therapist/src/main/java/com/projectTeam/therapist/model/UserDto.java
@@ -1,14 +1,18 @@
 package com.projectTeam.therapist.model;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Data
-public class UserDto {
+public class UserDto implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
@@ -33,13 +37,49 @@ public class UserDto {
 
     @OneToMany(mappedBy = "userDto", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PostCommentDto> userPostComments = new ArrayList<>();
-<<<<<<< HEAD
 
     @OneToMany(mappedBy = "userDto", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ReplyDto> relies = new ArrayList<>();
 
     @OneToMany(mappedBy = "userDto", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ReplyCommentDto> userReplyComments = new ArrayList<>();
-=======
->>>>>>> a007ff358 (feature-addThumbnailAvatar :: 로그인 유저 프로필, 썸네일 정보 가져오기 (3) - 왜 안될까...)
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+        for (RoleDto role : this.roles) {
+            auth.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.userEnabled;
+    }
 }
