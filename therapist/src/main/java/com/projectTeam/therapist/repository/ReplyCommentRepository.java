1776350d12e260a92311ea3899bd3cb19349a697
jungyr24 <jungyr24@hanmail.net>
2021-08-10 22:12:00 +0900

    replyCommentDto 생성 및 join, replyComment관련 기능 구현

--- therapist/src/main/java/com/projectTeam/therapist/repository/ReplyCommentRepository.java
+++ therapist/src/main/java/com/projectTeam/therapist/repository/ReplyCommentRepository.java
@@ -1,13 +1,13 @@
 package com.projectTeam.therapist.repository;
 
-import com.projectTeam.therapist.model.ReplyDto;
+import com.projectTeam.therapist.model.ReplyCommentDto;
 import com.projectTeam.therapist.model.UserDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReplyRepository extends JpaRepository<ReplyDto, Long> {
+public interface ReplyCommentRepository extends JpaRepository<ReplyCommentDto, Long> {
 
-    Page<ReplyDto> findByUserDto(UserDto userDto, Pageable pageable);
+    Page<ReplyCommentDto> findByUserDto(UserDto userDto, Pageable pageable);
 
 }
