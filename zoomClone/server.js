8750849ea47b1b9d2db2486bedfdc60a9c6b82ea
Heongilee <gjsrl1@gmail.com>
2021-08-11 19:41:04 +0900

    feature/webRtc_socketIo :: 주석

--- zoomClone/server.js
+++ zoomClone/server.js
@@ -4,6 +4,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4:uuidV4 } = require('uuid')
 
+// 제발 되라...
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
