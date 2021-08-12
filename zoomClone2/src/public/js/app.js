e02687c4550f921a808dc822ec373d16d196526c
Heongilee <gjsrl1@gmail.com>
2021-08-12 16:42:47 +0900

    feature/webRtc-chatWithWebsocket :: webSocket 메시지를 생성하여 서버와 클라이언트끼리 티키타카 테스트

--- zoomClone2/src/public/js/app.js
+++ zoomClone2/src/public/js/app.js
@@ -1,2 +1,23 @@
 // 여기 있는 socket이 back-end(src/server.js)와 실시간으로 소통할 수 있음.
+// window.location.host : 현재 접속중인 IP주소와 포트번호에 대한 정보가 담겨있음.
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 서버쪽의 WebSocket과 연결되는 이벤트가 발생하면, 해당 메서드를 수행한다.
+socket.addEventListener("open", () => {
+    console.log("서버와 연결되었습니다. ✅");
+})
+
+// 서버쪽의 WebSocket의 메시지를 받는 이벤트가 발생하면, 해당 메서드를 수행한다.
+socket.addEventListener("message", (message) => {
+    console.log("New message : ", message.data, "from the server.js");
+})
+
+// 서버쪽의 WebSocket과 연결이 끊기는 이벤트가 발생하면, 해당 메서드를 수행한다.
+socket.addEventListener("close", () => {
+    console.log("서버와 연결이 끊겼습니다. ❌");
+})
+
+// 1초 뒤에 해당 메서드를 실행.
+setTimeout(() => {
+    socket.send("hello from the browser!!");
+}, 1000)
--- zoomClone2/src/server.js
+++ zoomClone2/src/server.js
@@ -21,16 +21,29 @@ const handleListen = () => console.log(`Listening on http://localhost:3000 || ws
 const server = http.createServer(app);                      // express app을 이용해서 http 서버를 만듦.
 const wss = new WebSocket.Server({ server });               // http server위에 webSocket서버를 만듦.
 
+//////////////! depricated //////////////
+/* function handleConnection(socket) {
+    console.log(socket)
+} */
+////////////////////////////////////////!
+
+// wss의 on 메서드에서는 이벤트("connection")가 발생하기를 기다렸다가, 해당 메서드(handleConnection)를 수행함. -> 익명 함수(anonymous function)로 전환.
+// 
 // socket : 연결된 브라우저와의 연락(contact)라인, 연결된 어떤 다른 사람.
 // 여기 있는 socket이 front-end(src/public/js/app.js)와 실시간으로 소통할 수 있음.
-function handleConnection(socket) {
-    console.log(socket)
-}
+wss.on("connection", (socket) => {
+    console.log("브라우저와 연결되었습니다. ✅");
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    });
+    socket.on("close", () => {
+        console.log("브라우저와 연결이 끊겼습니다. ❌")
+    });
+    socket.send("안녕!!!");
 
-// wss의 on 메서드에서는 이벤트("connection")가 발생하기를 기다렸다가, 해당 메서드(handleConnection)를 수행함.
-wss.on("connection", handleConnection)
+})
 
-server.listen(3000, handleListen);  // http server위에 WebSocket 서버를 띄우는 방식. (이렇게 해야 views static files, home, redirection을 지원받을 수 있기 때문.)
+server.listen(3000, handleListen);  // http server위에 WebSocket 서버를 띄우는 방식. (이렇게 해야 views, static files, home, redirection을 지원받을 수 있기 때문.)
 // app.listen(3000);                // http 서버만 띄우는 방식.
 /////////////////////////////////////////////////////////////////////////////////// 
 // $ npm run dev
