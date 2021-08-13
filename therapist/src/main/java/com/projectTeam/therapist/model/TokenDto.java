18131518398da363e38837a11cd16405c4e14f42
yura <jungyr24@hanmail.net>
2021-08-13 14:36:37 +0900

    feature/jwt :: JWT 토큰 이용하여 사용자 정보 조회 (#53)
    * Security 기본 설정
    
    * add jwt library to gradle
    
    * JWT 관련 코드 추가
    
    * 관련 DTO 생성 및 업데이트
    
    * 로그인 관련 구현
    
    * 회원 정보 조회 with Authorization
    
    * 회원 가입 관련 구현
    
    * 회원가입 수정
    
    * 자잘한 코드 수정
    
    Co-authored-by: Heongi Lee <55871242+Heongilee@users.noreply.github.com>

---
+++ therapist/src/main/java/com/projectTeam/therapist/model/TokenDto.java
@@ -0,0 +1,14 @@
+package com.projectTeam.therapist.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+
+}
