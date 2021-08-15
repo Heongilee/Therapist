dc67922ba21742147e8dca727e61ced847fcb771
Heongilee <gjsrl1@gmail.com>
2021-08-15 20:45:27 +0900

    feature/webRtc-socketIo :: 방에 있는 사용자와 메시지 주고받기

--- zoomClone2/src/public/js/app.js
+++ zoomClone2/src/public/js/app.js
@@ -1,10 +1,12 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 room.hidden = true;
+const room_form = room.querySelector("form");
+const welcome_form = welcome.querySelector("form");
 
+// 현재 클라이언트가 접속한 방의 이름
 let roomName;
 
 function addMessage(msg) {
@@ -17,14 +19,28 @@ function addMessage(msg) {
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
-
+    
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+    
+    room_form.addEventListener("submit", handleMessageSubmit);
 }
 
-function handleRoomSubmit(event) {
+function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = form.querySelector("input");
+    const input = room_form.querySelector("input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value="";
+}
+
+
+welcome_form.addEventListener("submit", handleWelcomeSubmit);
+function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcome_form.querySelector("input");
 
     // 서버측에 메시지를 보낸다. (emit)
     // arg1 : 'room'이라는 이벤트 (이 이벤트는 내가 정하는 무엇이든지 가능. 단, 주고받을땐 확실히!)
@@ -35,8 +51,14 @@ function handleRoomSubmit(event) {
     input.value="";
 }
 
-form.addEventListener("submit", handleRoomSubmit);
 
 socket.on("welcome", () => {
-    addMessage("someone joined!");
+    addMessage("someone joined 🙋🏻‍♂️!");
 });
+
+socket.on("bye", () => {
+    addMessage("someone left 😭!");
+});
+
+socket.on("new_message", addMessage);
+// socket.on("new_message", (msg) => {addMessage(msg)}); // 이것과 같음.
