cf76d3cc2acd06f5eba488a91d1c51372a673743
yura <jungyr24@hanmail.net>
2021-08-16 14:16:54 +0900

    feature/jwt :: jwt 토큰 적용 및 grade API 구현  (#63)
    * feature/jwt :: JWT 토큰 이용하여 사용자 정보 조회 (#53)
    
    * Security 기본 설정
    
    * add jwt library to gradle
    
    * JWT 관련 코드 추가
    
    * 관련 DTO 생성 및 업데이트
    
    * 로그인 관련 구현
    
    * 회원 정보 조회 with Authorization
    
    * 회원 가입 관련 구현
    
    * 회원가입 수정
    
    * 자잘한 코드 수정
    
    Co-authored-by: Heongi Lee <55871242+Heongilee@users.noreply.github.com>
    
    * 평점 매기기 API 구현 (#62)
    
    * 불필요한 코드 제거
    
    * 답글 별점 매기기 및 별점에 따른 사용자 grade 업데이트
    
    Co-authored-by: Heongi Lee <55871242+Heongilee@users.noreply.github.com>

--- therapist/src/main/java/com/projectTeam/therapist/userService/AuthController.java
+++ therapist/src/main/java/com/projectTeam/therapist/userService/AuthController.java
@@ -1,21 +1,38 @@
 package com.projectTeam.therapist.userService;
 
+import com.projectTeam.therapist.jwt.JwtFilter;
+import com.projectTeam.therapist.jwt.TokenProvider;
+import com.projectTeam.therapist.model.LoginDto;
+import com.projectTeam.therapist.model.TokenDto;
 import com.projectTeam.therapist.model.UserDto;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.validation.Valid;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private UserService userService;
+
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public AuthController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.tokenProvider = tokenProvider;
+    }
 
     @GetMapping("/kakao/callback")
     public ResponseEntity<String> kakaoCallback(String code) { // @ResponseBody : Data를 리턴해주는 컨트롤러 함수
@@ -24,12 +41,20 @@ public class AuthController {
 
         RestTemplate rt = new RestTemplate();
 
-        UserDto newUser = new UserDto();
-        newUser.setUserName(userMap.get("username"));
-        newUser.setUserPassword(userMap.get("password"));
-        newUser.setUserProfileImage(userMap.get("profile_image"));
-        newUser.setUserThumbnailImage(userMap.get("thumbnail_image"));
-        newUser.setUserEnabled(true);
+        // builder 이용하여 UserDto 필드 초기화
+        UserDto newUser = UserDto.builder()
+                .userName(userMap.get("username"))
+                .userPassword(userMap.get("password"))
+                .userProfileImage(userMap.get("profile_image"))
+                .userThumbnailImage(userMap.get("thumbnail_image"))
+                .userEnabled(true)
+                .build();
+//        UserDto newUser = new UserDto();
+//        newUser.setUserName(userMap.get("username"));
+//        newUser.setUserPassword(userMap.get("password"));
+//        newUser.setUserProfileImage(userMap.get("profile_image"));
+//        newUser.setUserThumbnailImage(userMap.get("thumbnail_image"));
+//        newUser.setUserEnabled(true);
 
         HttpEntity<UserDto> requestEntity = new HttpEntity<>(newUser);
         rt.exchange(
@@ -45,5 +70,23 @@ public class AuthController {
             add("username", kakaoUser.getUserName());
             add("password", userMap.get("password"));
         }});
+    }
+
+    // jwt 토큰 받아오기
+    @PostMapping("/authenticate")
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
     }
 }
