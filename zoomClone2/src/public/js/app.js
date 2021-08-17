c6ca32cf6dfe9beab8f598c9c4719387af95a6cf
Heongilee <gjsrl1@gmail.com>
2021-08-18 00:35:42 +0900

    feature/webRtc-videoCall :: enumerateDevices()를 이용하여 내 기기의 비디오&오디오 장치 가져오기

--- zoomClone2/src/public/js/app.js
+++ zoomClone2/src/public/js/app.js
@@ -1,44 +1,75 @@
+// https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia
 const socket = io();
 
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+const audiosSelect = document.getElementById("audios");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-// https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia
-async function getMedia() {
-    try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        });
-        myFace.srcObject = myStream;
-    } catch (e) {
-        console.log(e);
-    }
+async function getCamerasAndAudios() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const audios = devices.filter((device) => device.kind === "audioinput");
+    cameras.forEach((camera) => {
+        const option = document.createElement("option");
+        option.value = camera.deviceId;
+        option.innerText = camera.label;
+        camerasSelect.appendChild(option);
+    });
+    audios.forEach((audio) => {
+        const option = document.createElement("option");
+        option.value = audio.deviceId;
+        option.innerText = audio.label;
+        audiosSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+    await getCamerasAndAudios();
+  } catch (e) {
+    console.log(e);
+  }
+}
 getMedia();
+
 function handleMuteClick() {
-    if(!muted) {
-        muteBtn.innerText = "Unmute";
-        muted = true;
-    } else {
-        muteBtn.innerText = "Mute";
-        muted = false;
-    }
+  myStream.getAudioTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
 }
 function handleCameraClick() {
-    if(cameraOff) {
-        cameraBtn.innerText = "Turn Camera Off";
-        cameraOff = false;
-    } else {
-        cameraBtn.innerText = "Turn Camera On";
-        cameraOff = true;
-    }
+  myStream.getVideoTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
