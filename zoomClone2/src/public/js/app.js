b2e0b904bfb01c299560ecdccc6501849fc36463
Heongilee <gjsrl1@gmail.com>
2021-08-17 23:54:41 +0900

    feature/webRtc-videoCall :: 내 음성과 비디오스트림 가져오기

--- zoomClone2/src/public/js/app.js
+++ zoomClone2/src/public/js/app.js
@@ -1,1 +1,45 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+// https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia
+async function getMedia() {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
+        myFace.srcObject = myStream;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+getMedia();
+function handleMuteClick() {
+    if(!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+function handleCameraClick() {
+    if(cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
