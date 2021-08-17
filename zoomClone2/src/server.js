a984af122753258d6f6978002de58a1c2e59f390
Heongilee <gjsrl1@gmail.com>
2021-08-17 23:54:41 +0900

    feature/webRtc-videoCall :: 내 음성과 비디오스트림 가져오기

--- zoomClone2/src/server.js
+++ zoomClone2/src/server.js
@@ -3,6 +3,14 @@ import SocketIO from "socket.io";
 import express from "express";
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res) => {
+  res.render("home");
+});
+
 app.get("/*", (_, res) => {
   res.redirect("/");
 });
