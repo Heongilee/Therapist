cd744d75f8569254093f50e2f0353310d767d8fd
Heongilee <gjsrl1@gmail.com>
2021-08-20 01:02:50 +0900

    feature/webRtc-videoCall :: Google의 임시 STUN서버를 이용해서 핸드폰으로 테스트해보기 with localtunnel

--- zoomClone2/src/server.js
+++ zoomClone2/src/server.js
@@ -21,18 +21,20 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen);
 
 io.on("connection", (socket) => {
-  ////////////////////////////////////////////* socket.on() Client -> Server *////////////////////////////////////////////
   // 해당 소켓을 방에 참가시킨다.
-  socket.on("join_room", (roomName, done) => {
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    done();
     socket.to(roomName).emit("welcome");
   });
   socket.on("offer", (offer, roomName) => {
     // peerA에게 받은 Offer를 roomName에 있는 모든 socket에게 전송함.
     socket.to(roomName).emit("offer", offer);
   });
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*
-  ////////////////////////////////////////////* socket.emit() Client <- Server *////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*
+  socket.on("answer", (answer, roomName) => {
+    // peerA에게 받은 Offer를 roomName에 있는 모든 socket에게 전송함.
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
 });
