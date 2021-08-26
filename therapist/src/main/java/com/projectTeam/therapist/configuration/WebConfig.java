dc300dbef9c05d29d947111f71f2789f29e308c7
yura <jungyr24@hanmail.net>
2021-08-26 23:56:33 +0900

    fix CORS error (#87)

---
+++ therapist/src/main/java/com/projectTeam/therapist/configuration/WebConfig.java
@@ -0,0 +1,13 @@
+package com.projectTeam.therapist.configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://000.000.00.000:3000");
+    }
+}
--- therapist/src/main/java/com/projectTeam/therapist/restService/CommentApiController.java
+++ therapist/src/main/java/com/projectTeam/therapist/restService/CommentApiController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 class CommentApiController {
