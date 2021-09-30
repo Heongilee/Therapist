828c39c56bad401a842b6b3d96e8e4d1300336df
Heongilee <gjsrl1@gmail.com>
2021-09-30 15:19:00 +0900

    feature/openviduSession-hotfix :: REST API 키값 오타 수정 + CORS 에러 해결(api/openvidu/**)

--- therapist/src/main/java/com/projectTeam/therapist/configuration/WebSecurityConfig.java
+++ therapist/src/main/java/com/projectTeam/therapist/configuration/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
+                    .cors().and()
                     .antMatcher("/api/openvidu/**")
                     .csrf().disable().formLogin().disable()
                     .authorizeRequests(authorize -> authorize.anyRequest().hasRole("OPENVIDU"))
