6599b55ba918054744c83908319631ac1a496f39
Heongilee <gjsrl1@gmail.com>
2021-10-02 17:22:13 +0900

    feature/openVidu-countingConnectionObject :: 연결객체 수 제어하기(numConnectedObject) + 방장 이름(sessionModerator)컬럼 추가하고 업데이트하는 API(/api/openvidu/session/<SESSION_ID>)

--- therapist/src/main/java/com/projectTeam/therapist/restService/OpenviduApiController.java
+++ therapist/src/main/java/com/projectTeam/therapist/restService/OpenviduApiController.java
@@ -30,6 +30,24 @@ public class OpenviduApiController {
         return sessionService.retrieveSession(sessionId);
     }
 
+    // PUT 연결객체 참가시켜 카운트 증가 API
+    @PutMapping("/session/{sessionId}/enter")
+    JSONObject incrementConnectedObject(@PathVariable Long sessionId) {
+        return sessionService.incrementConnectedObject(sessionId);
+    }
+
+    // PUT 연결객체 참가시켜 카운트 감소 API
+    @PutMapping("/session/{sessionId}/exit")
+    JSONObject decrementConnectedObject(@PathVariable Long sessionId) {
+        return sessionService.decrementConnectedObject(sessionId);
+    }
+
+    // PUT 방장 교체하는 API
+    @PutMapping("/session/{sessionId}")
+    SessionDto updateSessionModerator(@PathVariable Long sessionId, @RequestBody JSONObject requestBody) {
+        return sessionService.updateSessionModerator(sessionId, requestBody);
+    }
+
     // DELETE 방식으로 해당 세션 종료시키기
     @DeleteMapping("/session/{sessionId}")
     void removeSession(@PathVariable Long sessionId) {
