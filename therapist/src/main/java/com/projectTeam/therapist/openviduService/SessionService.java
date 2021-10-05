784a18b832e54a4ae7a67946d17d4624b713d79c
Heongilee <gjsrl1@gmail.com>
2021-10-05 19:06:17 +0900

    feature/openVidu-modifyRestApi :: 방장 교체하는 API 구현

--- therapist/src/main/java/com/projectTeam/therapist/openviduService/SessionService.java
+++ therapist/src/main/java/com/projectTeam/therapist/openviduService/SessionService.java
@@ -1,15 +1,26 @@
 package com.projectTeam.therapist.openviduService;
 
-import com.projectTeam.therapist.RestTemplate.ApiService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projectTeam.therapist.RestTemplate.HttpClientConfig;
 import com.projectTeam.therapist.model.MediaServerConnectionPool;
 import com.projectTeam.therapist.model.SessionDto;
 import com.projectTeam.therapist.repository.SessionRepository;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @Service
@@ -18,8 +29,8 @@ public class SessionService {
     private SessionRepository sessionRepository;
     @Autowired
     private MediaServerConnectionPool mscp;
-    @Autowired
-    private ApiService<Response> apiService;
+    @Value("${rest.ignore_ssl}")
+    private Boolean IGNORE_SSL;
 
 
     public SessionDto createSession(JSONObject requestBody) {
@@ -91,13 +102,44 @@ public class SessionService {
         return result;
     }
 
-    public SessionDto updateSessionModerator(Long sessionId, JSONObject requestBody) {
-        // https://ec2-18-234-102-149.compute-1.amazonaws.com/openvidu/api/sessions/354
-        String url = mscp.PROTOCOL + "://" + mscp.DOMAIN + mscp.URI_PREFIX + "/sessions/" + String.valueOf(sessionId);
+    public SessionDto updateSessionModerator(String sessionId, JSONObject requestBody) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException, JsonProcessingException {
+        SessionDto sessionDto = sessionRepository.findById(Long.valueOf(sessionId)).orElse(null);
 
-//        SessionDto sessionDto = sessionRepository.findById(sessionId).orElse(null);
-//
-//        sessionDto.setSessionModerator((String) requestBody.get("sessionModerator"));
-//        return sessionRepository.save(sessionDto);
+        // https://ec2-18-234-102-149.compute-1.amazonaws.com/openvidu/api/sessions/354
+        // Authorization Basic T1BFTlZJRFVBUFA6dGhlcmFwaXN0
+        String url = mscp.PROTOCOL + "://" + mscp.DOMAIN + mscp.URI_PREFIX + "/sessions/" + sessionId;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        headers.add("Authorization", "Basic T1BFTlZJRFVBUFA6dGhlcmFwaXN0");
+        HttpEntity<MultiValueMap<String, String>> httpRequest = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response2;
+        response2 = new RestTemplate(HttpClientConfig.trustRequestFactory()).exchange(
+                url,
+                HttpMethod.GET,
+                httpRequest,
+                String.class
+        );
+        System.out.println("body = " + response2.getBody());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = null;
+        root = objectMapper.readTree(response2.getBody());
+
+        JsonNode connections = root.path("connections");
+        for(int i = 0; i < Integer.parseInt(String.valueOf(connections.path("numberOfElements"))); i++) {
+            JsonNode connection = objectMapper.readTree(String.valueOf(connections.path("content").path(i).path("clientData")));
+
+            String clientData = connection.toString();
+            int lt = 19;
+            int rt = 19;
+            while (clientData.charAt(rt) != '\\') { rt ++; }
+            clientData = clientData.substring(lt, rt);
+            if(!clientData.equals(requestBody.get("sessionModerator"))) {
+                sessionDto.setSessionModerator(clientData);
+                return sessionRepository.save(sessionDto);
+            }
+        }
+        return new SessionDto();
     }
 }
