6144709a7d84c2d880be9444e6320929433a170f
Heongilee <gjsrl1@gmail.com>
2021-10-05 19:06:17 +0900

    feature/openVidu-modifyRestApi :: 방장 교체하는 API 구현

---
+++ therapist/src/main/java/com/projectTeam/therapist/RestTemplate/HttpClientConfig.java
@@ -0,0 +1,48 @@
+package com.projectTeam.therapist.RestTemplate;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.TrustStrategy;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+/**
+ * @author reference-m1
+ * @desc HttpClientConfig
+ */
+public class HttpClientConfig {
+
+    /**
+     * restTemplate SSL ignore
+     * @return HttpComponentsClientHttpRequestFactory
+     * @throws KeyStoreException
+     * @throws NoSuchAlgorithmException
+     * @throws KeyManagementException
+     */
+    public static HttpComponentsClientHttpRequestFactory trustRequestFactory() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        return requestFactory;
+    }
+}
