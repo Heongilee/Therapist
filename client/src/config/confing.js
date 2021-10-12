6cae7b08739f9b93a9878e90578ae63434e84ef9
seounjin <39517396+seounjin@users.noreply.github.com>
2021-10-12 18:16:42 +0900

    주소 change (#156)

--- client/src/config/confing.js
+++ client/src/config/confing.js
@@ -1,6 +1,6 @@
-export const URL ='http://ec2-18-235-96-9.compute-1.amazonaws.com/api';
-export const Auth ='http://ec2-18-235-96-9.compute-1.amazonaws.com';
-export const SOCKET_URL = 'ws://ec2-18-235-96-9.compute-1.amazonaws.com/socket';
+export const URL ='https://therapis2gether.com/api';
+export const Auth ='https://therapis2gether.com';
+export const SOCKET_URL = 'wss://therapis2gether.com/socket';
 
 
 export const OPENVIDU_SERVER_URL = 'https://ec2-18-234-102-149.compute-1.amazonaws.com';
