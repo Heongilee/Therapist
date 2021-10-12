1ca31b7f885f50ad643b93eaf7c2db6d9ab591ee
Heongi Lee <55871242+Heongilee@users.noreply.github.com>
2021-10-12 19:12:28 +0900

    Update MediaServerConnectionPool.java

--- therapist/src/main/java/com/projectTeam/therapist/model/MediaServerConnectionPool.java
+++ therapist/src/main/java/com/projectTeam/therapist/model/MediaServerConnectionPool.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class MediaServerConnectionPool {
     public static final String PROTOCOL= "https";
-    public static final String DOMAIN= "ec2-18-234-102-149.compute-1.amazonaws.com";
+    public static final String DOMAIN= "ec2-3-87-2-20.compute-1.amazonaws.com";
     public static final String URI_PREFIX="/openvidu/api";
 }
