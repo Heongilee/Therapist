6e9e6db9500e812e295c767f6dca96f841376133
Heongi Lee <55871242+Heongilee@users.noreply.github.com>
2021-10-19 16:56:22 +0900

    Feature/time zone (#171)
    * feature/timeZone :: 타임존 Asia/Seoul로 변경
    
    * feature/timeZone :: 타임존 Asia/Seoul로 변경 (수정)

--- therapist/src/main/java/com/projectTeam/therapist/model/ReplyCommentDto.java
+++ therapist/src/main/java/com/projectTeam/therapist/model/ReplyCommentDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Entity
 @Data
@@ -33,7 +34,9 @@ public class ReplyCommentDto {
     // DB에 INSERT를 날리기전에 해당 메서드를 먼저 실행하여 현재시각과 업데이트 시각을 설정한다.
     @PrePersist
     public void prePersist() {
-        LocalDateTime now = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+        // utc
+//        LocalDateTime now = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+        LocalDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime();
         this.commentCreatedAt = now;
         this.commentUpdatedAt = now;
     }
@@ -41,6 +44,8 @@ public class ReplyCommentDto {
     // 해당 테이블로 UPDATE문이 들어왔을때 트리거처럼 호출되며 업데이트 시각을 현재시각으로 설정한다.
     @PreUpdate
     public void preUpdate() {
-        this.commentUpdatedAt = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+        // utc
+//        this.commentUpdatedAt = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+        this.commentUpdatedAt = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime();
     }
 }
