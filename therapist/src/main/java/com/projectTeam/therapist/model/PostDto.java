54a74aa10e955ccd680e874ff91405a26c717e5d
Heongilee <gjsrl1@gmail.com>
2021-10-19 16:31:19 +0900

    feature/timeZone :: 타임존 Asia/Seoul로 변경

--- therapist/src/main/java/com/projectTeam/therapist/model/PostDto.java
+++ therapist/src/main/java/com/projectTeam/therapist/model/PostDto.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,7 +50,7 @@ public class PostDto {
     // DB에 INSERT를 날리기전에 해당 메서드를 먼저 실행하여 현재시각과 업데이트 시각을 설정한다.
     @PrePersist
     public void prePersist() {
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
         this.postCreatedAt = now;
         this.postUpdatedAt = now;
     }
@@ -57,6 +58,6 @@ public class PostDto {
     // 해당 테이블로 UPDATE문이 들어왔을때 트리거처럼 호출되며 업데이트 시각을 현재시각으로 설정한다.
     @PreUpdate
     public void preUpdate() {
-        this.postUpdatedAt = LocalDateTime.now();
+        this.postUpdatedAt = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
     }
 }
